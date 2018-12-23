c39c1e20de062a9a32fd0afdfa33ad81b46c4e57
ninatu <nina.tuluptceva@philips.com>
2018-12-23 17:40:05 +0300

    Add mutexs, Debug detach

--- problem3/subtask2/main.cpp
+++ problem3/subtask2/main.cpp
@@ -1,7 +1,12 @@
 #include <memory>
 #include <unordered_set>
 #include <unordered_map>
+#include <map>
 #include <vector>
+#include <mutex>
+#include <iostream>
+#include <list>
+#include <atomic>
 
 
 class IShop;
@@ -36,11 +41,12 @@ public:
     virtual ~IProduct() = default;
 
     friend IShop;
-private:
-    std::string _name;
-    double _price;
-    bool _on_sale;
-    std::vector<std::weak_ptr<IShop>> _shops;
+
+protected:
+    const std::string _name;
+    std::atomic<double> _price;
+    std::atomic_bool _on_sale;
+    std::list<std::weak_ptr<IShop>> _shops;
 
     void NotifyShops();
 };
@@ -50,58 +56,67 @@ class IShop {
 public:
     IShop(const std::string &name): _name(name) {};
     virtual ~IShop() = default;
+    const std::unordered_map<std::string, double> & GetProductOnSale() {
+        return _products_on_sale;
+    };
+
 
     friend void IProduct::Attach(std::weak_ptr <IShop>);
     friend void IProduct::Detach(std::weak_ptr <IShop> shop);
     friend void IProduct::NotifyShops();
 
-private:
+protected:
+    std::mutex _on_change;
+
     std::string _name;
-    std::unordered_set<std::shared_ptr<IProduct>> _products;
     std::unordered_map<std::string, double> _products_on_sale;
 
-    enum shopAction {START_SALE, STOP_SALE, CHANGE_PRICE};
 
-    void UpdateProduct(std::shared_ptr<IProduct> prod) {
+    void UpdateProduct(std::shared_ptr <IProduct> prod) {
+        std::lock_guard<std::mutex> lock(_on_change);
+
         if (prod->OnSale()) {
             _products_on_sale.insert_or_assign(prod->GetName(), prod->GetPrice());
-//            Announcement("")
         } else {
             _products_on_sale.erase(prod->GetName());
         }
     }
 
-    void AddProduct(std::shared_ptr<IProduct> prod) {
-        _products.insert(prod);
-        UpdateProduct(prod);
-    }
-
     void DeleteProduct(std::shared_ptr<IProduct> prod) {
-        _products.erase(prod);
-        UpdateProduct(prod);
+        std::lock_guard<std::mutex> lock(_on_change);
+         _products_on_sale.erase(prod->GetName());
     }
-
-//    void Announcement(shopAction action, std::shared_ptr<IProduct> prod) {
-//        std::cout << "The shop '" << _name << "' " <<
-//    }
-
-
 };
 
 
 void IProduct::Attach(std::weak_ptr <IShop> shop)  {
     auto shop_ptr = shop.lock();
     if (shop_ptr != nullptr) {
-        shop_ptr->AddProduct(this->shared_from_this());
+        _shops.push_back(shop);
+        shop_ptr->UpdateProduct(this->shared_from_this());
     }
 };
+
 
 void IProduct::Detach(std::weak_ptr <IShop> shop)  {
     auto shop_ptr = shop.lock();
     if (shop_ptr != nullptr) {
+        // delete from this shop
         shop_ptr->DeleteProduct(this->shared_from_this());
+
+        // delete from shops list
+        for (auto iter=_shops.begin(); iter != _shops.end(); iter++) {
+            auto track_shop_ptr = iter->lock();
+            if (track_shop_ptr != nullptr) {
+                if (shop_ptr == track_shop_ptr) {
+                    _shops.erase(iter);
+                    break;
+                }
+            }
+        }
     }
 };
+
 
 void IProduct::NotifyShops() {
     for (auto shop: _shops) {
@@ -126,14 +141,40 @@ private:
 };
 
 
+class ClassicShop: public IShop {
+public:
+    ClassicShop(std::string name): IShop(name) {};
+    void AnnounceSales() {
+        std::lock_guard<std::mutex> lock(_on_change);
+
+        if (_products_on_sale.size()) {
+            std::cout << "The shop '" << _name << "' sales following goods:  " << std::endl;
+            for (auto prod: _products_on_sale) {
+                std::cout << "    " << prod.first << ": " << prod.second << "$" << std::endl;
+            }
+        } else {
+            std::cout << "The shop '" << _name << "' don't sale anything now." << std::endl;
+        }
+    }
+};
+
 
 
 int main() {
-    auto toy_shop_ptr = std::make_shared<IShop>(std::string("Best Toys"));
+    auto toy_shop_ptr = std::make_shared<ClassicShop>(std::string("Best Toys"));
 
-    TeddyBear bear(100, SMALL, "super_cool");
+    auto bear = std::make_shared<TeddyBear>(100, SMALL, "super_cool");
 
-    bear.Attach(toy_shop_ptr);
-    bear.Detach(toy_shop_ptr);
+    bear->Attach(toy_shop_ptr);
+    toy_shop_ptr->AnnounceSales();
+
+    bear->StartSales();
+    toy_shop_ptr->AnnounceSales();
+
+    bear->Detach(toy_shop_ptr);
+    toy_shop_ptr->AnnounceSales();
+
+    bear->Attach(toy_shop_ptr);
+    toy_shop_ptr->AnnounceSales();
 
 }
